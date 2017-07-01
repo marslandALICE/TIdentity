fdb2f97a3a538bed9fc340c3e25065f37a9ced51
marslandALICE <mesut.arslandok@cern.ch>
2017-07-01 23:40:36 +0200

    First version

---
+++ test/runTest.sh
@@ -0,0 +1,5 @@
+#./testIdenTest PbPb/Real/PbPb_cRows_80_16EtaBin_mombin20MeV_LooseCuts_PxPyPz/SubSamples/cent_10.0/SubSample_cent10.00_ss1.root PbPb/Real/PbPb_cRows_80_16EtaBin_mombin20MeV_LooseCuts_PxPyPz/ParamTrees/FitResults_piS1_kaS1_prS1_piK1_pikaprKauto.root 10 1 6 -0.8 0.8 -1
+
+./testIdenTest PbPb/Real/Systematics_cRows_80_16EtaBin_mombin20MeV/SubSamples/cent_0.0/SubSample_cent0.00_ss1.root PbPb/Real/Systematics_cRows_80_16EtaBin_mombin20MeV/ParamTrees/FitResults_piS0.7_kaS0.5_prS0.5_pikaprKauto.root 0 1 6 0 0 1 0  0 0
+
+#./testIdenTest PbPb/Real/PbPb_cRows_80_16EtaBin_mombin20MeV_TightCuts_PxPyPz/SubSamples/cent_40.0/SubSample_cent40.00_ss0.root PbPb/Real/PbPb_cRows_80_16EtaBin_mombin20MeV_TightCuts/ParamTrees/ParamTree_piS0.7_kaS0.5_prS0.5_Kurtosis2_fullStat.root 40 1 6 -0.8 0.8 -1
